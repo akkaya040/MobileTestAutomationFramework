93c7f1ae6f025e286ccabd2b19562ec15f754327
Kurtulus <akkaya040@gmail.com>
2022-12-29 17:54:39 +0300

    Driver Properties Were Changed.

--- src/main/java/utils/ThreadLocalDriver.java
+++ src/main/java/utils/ThreadLocalDriver.java
@@ -6,12 +6,17 @@ import io.appium.java_client.android.AndroidDriver;
 public class ThreadLocalDriver {
     private static final ThreadLocal<AndroidDriver<MobileElement>> driver = new ThreadLocal<>();
 
-    public static synchronized void setDriver(AndroidDriver<MobileElement> driver) {
-        ThreadLocalDriver.driver.set(driver);
+    public static synchronized void setDriver(AndroidDriver<MobileElement> androidDriver) {
+        driver.set(androidDriver);
     }
 
     public static synchronized AndroidDriver<MobileElement> getDriver() {
 
         return driver.get();
     }
+
+    public static synchronized void killDriver() {
+        driver.get().quit();
+    }
+
 }
